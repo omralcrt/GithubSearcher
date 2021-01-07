214789abbcb6cd274fb8da473a54488ab26ec1f7
omralcorut <omralcrt@gmail.com>
2021-01-07 08:24:36 +0300

    Implement Search Field

--- app/src/main/java/com/omralcorut/githubsearcher/ui/search/SearchFragment.kt
+++ app/src/main/java/com/omralcorut/githubsearcher/ui/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -29,6 +30,7 @@ class SearchFragment : Fragment() {
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         setAdapter()
+        initSearchView()
         viewModelObserver()
         return binding.root
     }
@@ -50,6 +52,24 @@ class SearchFragment : Fragment() {
         binding.recyclerViewSearch.adapter = searchEpoxyController.adapter
     }
 
+    private fun initSearchView() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                if (newText.isEmpty()) {
+                    searchViewModel.clearResults()
+                } else {
+                    searchViewModel.getUsers(newText)
+                    searchViewModel.getRepositories(newText)
+                }
+                return false
+            }
+        })
+    }
+
     private fun viewModelObserver() {
         searchViewModel.users.observe(viewLifecycleOwner, {
             searchEpoxyController.setData(
@@ -64,6 +84,10 @@ class SearchFragment : Fragment() {
                 searchViewModel.users.value,
                 searchViewModel.repositories.value
             )
+        })
+
+        searchViewModel.isResultEmpty.observe(viewLifecycleOwner, {
+            binding.textViewNoResult.visibility = if (it) View.VISIBLE else View.GONE
         })
     }
 
--- app/src/main/java/com/omralcorut/githubsearcher/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/omralcorut/githubsearcher/ui/search/SearchViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.*
 import com.omralcorut.githubsearcher.models.Repository
 import com.omralcorut.githubsearcher.models.User
 import com.omralcorut.githubsearcher.repository.SearchRepository
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
@@ -15,6 +16,7 @@ class SearchViewModel @ViewModelInject constructor(
 
     private val _users = MutableLiveData<List<User>>(listOf())
     private val _repositories = MutableLiveData<List<Repository>>(listOf())
+    private val _isResultEmpty = MutableLiveData<Boolean>(true)
 
     val users: LiveData<List<User>>
         get() = _users
@@ -22,25 +24,44 @@ class SearchViewModel @ViewModelInject constructor(
     val repositories: LiveData<List<Repository>>
         get() = _repositories
 
+    val isResultEmpty: LiveData<Boolean>
+        get() = _isResultEmpty
 
-    init {
-        getUsers()
-        getRepositories()
-    }
-
-    fun getUsers() {
+    fun getUsers(query: String) {
         viewModelScope.launch {
-            searchRepository.getUsers("username").map { it.items }.collect { items ->
-                _users.value = items
-            }
+            searchRepository.getUsers(query).map { it.items }
+                .catch { exception ->
+                    _users.value = listOf()
+                    checkResultState()
+                }
+                .collect { items ->
+                    _users.value = items
+                    checkResultState()
+                }
         }
     }
 
-    fun getRepositories() {
+    fun getRepositories(query: String) {
         viewModelScope.launch {
-            searchRepository.getRepositories("reponame").map { it.items }.collect { items ->
-                _repositories.value = items
-            }
+            searchRepository.getRepositories(query).map { it.items }
+                .catch { exception ->
+                    _repositories.value = listOf()
+                    checkResultState()
+                }
+                .collect { items ->
+                    _repositories.value = items
+                    checkResultState()
+                }
         }
+    }
+
+    fun clearResults() {
+        _users.value = listOf()
+        _repositories.value = listOf()
+        checkResultState()
+    }
+
+    private fun checkResultState() {
+        _isResultEmpty.value = users.value!!.isEmpty() && repositories.value!!.isEmpty()
     }
 }
